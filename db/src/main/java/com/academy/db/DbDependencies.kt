ce16008343d24d07853ce5755006ecee88ab13fd
Korolev, Alexey (ak3140) <ak3140@att.com>
2020-12-02 07:49:13 +0200

    DB module - MovieDatabase.kt + DbDependencies.kt

---
+++ db/src/main/java/com/academy/db/DbDependencies.kt
@@ -0,0 +1,15 @@
+package com.academy.db
+
+import android.content.Context
+import androidx.room.Room
+
+object DbDependencies {
+    private lateinit var roomDb: MovieDatabase
+    private fun getRoomDb(context: Context): MovieDatabase {
+        if (!::roomDb.isInitialized) roomDb =
+            Room.databaseBuilder(context, MovieDatabase::class.java, "movies_database").build()
+        return roomDb
+    }
+
+    private fun getMovieDao(context: Context): MovieDao = getRoomDb(context).movieDao()
+}
