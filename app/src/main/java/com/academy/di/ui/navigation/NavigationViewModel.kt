dd7a0fae645a7d55426dd3a364825097300064b3
Korolev, Alexey (ak3140) <ak3140@att.com>
2020-12-08 05:55:10 +0200

    Navigation refactoring

--- app/src/main/java/com/academy/di/ui/navigation/NavigationViewModel.kt
+++ app/src/main/java/com/academy/di/ui/navigation/NavigationViewModel.kt
@@ -2,23 +2,22 @@ package com.academy.di.ui.navigation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.NavDirections
 import androidx.navigation.fragment.FragmentNavigator
-import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.academy.db.model.Movie
 import com.academy.di.ui.home.HomeFragmentDirections
 import com.academy.di.utils.SingleLiveEvent
 
 class NavigationViewModel : ViewModel() {
-    private var navEvent = SingleLiveEvent<Pair<NavDirections, FragmentNavigator.Extras>>()
-    fun getNavEvent(): LiveData<Pair<NavDirections, FragmentNavigator.Extras>> = navEvent
+    private var navEvent = SingleLiveEvent<NavParams>()
+    fun getNavEvent(): LiveData<NavParams> = navEvent
 
+    // HomeFragment Actions
     fun onUserMovieClick(movie: Movie, extras: FragmentNavigator.Extras) {
         navEvent.value =
-            Pair(HomeFragmentDirections.actionHomeFragmentToDetailsFragment(movie), extras)
+            NavParams(HomeFragmentDirections.actionHomeFragmentToDetailsFragment(movie), extras)
     }
 
     fun onSettingsClick() {
-        navEvent.value = Pair(HomeFragmentDirections.actionHomeFragmentToSettingsFragment(), FragmentNavigatorExtras())
+        navEvent.value = NavParams(HomeFragmentDirections.actionHomeFragmentToSettingsFragment())
     }
 }
