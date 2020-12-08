ae8763bfc651fdd12ed3dd1eb298aa49e1552be4
Korolev, Alexey (ak3140) <ak3140@att.com>
2020-12-08 07:00:25 +0200

    Added SettingsRepo

--- app/src/main/java/com/academy/di/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/academy/di/ui/settings/SettingsFragment.kt
@@ -1,9 +1,15 @@
 package com.academy.di.ui.settings
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.academy.di.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
     private val viewModel: SettingsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
