f402011fdb2d9b6c3be6db8b3589afef6c60c6fc
Korolev, Alexey (ak3140) <ak3140@att.com>
2020-12-21 14:50:32 +0200

    Dagger 2 Integration - Added Step 3 TODOs

--- app/src/main/java/com/academy/di/di/Injector.kt
+++ app/src/main/java/com/academy/di/di/Injector.kt
@@ -12,13 +12,23 @@ import com.academy.network.di.NetworkModule
 object Injector {
     lateinit var appComponent: AppComponent
 
+    // TODO Step 3 - Create @SettingsScope (@Scope / @kotlin.annotation.Retention(AnnotationRetention.RUNTIME))
+    // TODO Step 3 - Create @SubComponent SettingsComponent with SettingsModule (use  @Subcomponent.Builder annotation)
+    // TODO Step 3 - Create a nullable variable for storing reference of SettingsComponent
+
     fun buildDaggerAppComponent(appContext: Context) {
         appComponent = DaggerAppComponent.builder()
             .moviesModule(MoviesModule())
             .dbModule(DbModule(appContext))
             .networkModule(NetworkModule())
             .dataStoreModule(DataStoreModule(appContext))
+            // TODO Step 3 - Remove settingsModule function call
             .settingsModule(SettingsModule())
             .build()
     }
+
+    // TODO Step 3 - Add a function that when settingsComponent variable is null will build it
+    //  by using addSettingsSubComponent function from AppComponent and do not forget to add a settingsModule.
+
+    // TODO Step 3 - Add a function that will clear (make it null) settingsComponent variable
 }
