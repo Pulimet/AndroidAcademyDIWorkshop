ee7171ff977226607957a03efb395aad1c6eda21
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2021-01-10 08:22:53 +0200

    FavoritesViewModel.kt - Clear Favorites Component

--- ui-favorites/src/main/java/com/academy/ui_favorites/FavoritesViewModel.kt
+++ ui-favorites/src/main/java/com/academy/ui_favorites/FavoritesViewModel.kt
@@ -3,9 +3,9 @@ package com.academy.ui_favorites
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.academy.ui_favorites.di.Dependencies
+import com.academy.navigation.DiHolder
 
-// TODO Step 5 - Nothing TODO, just note it was modified
+// TODO Step 5 - Nothing TODO, just note it was modified and FavoritesComponent cleared
 class FavoritesViewModel(private val favoritesRepo: FavoritesRepo) : ViewModel() {
     init {
         Log.w("Academy", "FavoritesViewModel init")
@@ -15,6 +15,7 @@ class FavoritesViewModel(private val favoritesRepo: FavoritesRepo) : ViewModel()
 
     override fun onCleared() {
         favoritesRepo.onCleared()
+        DiHolder.baseInjector.clearFavoritesComponent()
         Log.w("Academy", "FavoritesViewModel onCleared")
     }
 }
